836ceb49f25320d22a1da3af74b6c5e4b0a9310b
Noooile <elion.ft@gmail.com>
2025-09-23 15:23:16 +0200

    added q4 python file

---
+++ q4.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# CONSTANTS
+RADIUS = 6378 # m
+MAGNETIC_FIELD = 3.11e-5 # T
+
+ENERGY_ELECTRON = 100e3 * 1.602e-19 # J
+ENERGY_PROTON = 100 * 1.602e-19 # J
+
+
